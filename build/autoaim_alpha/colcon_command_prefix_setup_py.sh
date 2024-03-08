3e623a6d82b4986c82eb3bf66504e1f07cfa9829
ggbond <liyuxuan12345678@outlook.com>
2024-03-08 06:55:00 -0500

    a

--- build/autoaim_alpha/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/liyuxuan/vscode/pywork_linux/autoaim_ws/src/install/autoaim_interface/share/autoaim_interface/package.sh"
--- build/autoaim_interface/CMakeFiles/autoaim_interface.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/autoaim_interface"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/autoaim_interface.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
