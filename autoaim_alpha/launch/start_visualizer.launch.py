212587c82414b18d0671b708649348d4606137f8
ggbond <liyuxuan12345678@outlook.com>
2024-03-11 08:31:29 -0400

    a

--- autoaim_alpha/launch/start_visualizer.launch.py
+++ autoaim_alpha/launch/start_visualizer.launch.py
@@ -6,13 +6,13 @@ from launch.actions import ExecuteProcess
 package_name = 'autoaim_alpha'
 
 def generate_launch_description():
-    return LaunchDescription(
+    return LaunchDescription([
             Node(
             package=package_name,
             node_executable='node_visualizer',
             node_name='node_visualizer',
             output='screen'
-        )
+        )]
     )
 
 
