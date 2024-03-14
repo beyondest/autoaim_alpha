1690fd2decd61577734e51d775057c9543d0c424
ggbond <liyuxuan12345678@outlook.com>
2024-03-14 07:38:36 -0400

    a

--- autoaim_alpha/autoaim_alpha/node_decision_maker.py
+++ autoaim_alpha/autoaim_alpha/node_decision_maker.py
@@ -208,18 +208,15 @@ class Node_Decision_Maker(Node,Custom_Context_Obj):
         
         com_msg = ElectricsysState()
         
-        target_armor = self.decision_maker.choose_target()
-        next_yaw,next_pitch,fire_times = self.decision_maker.make_decision(target_armor) 
+        next_yaw,next_pitch,fire_times = self.decision_maker.make_decision() 
        
         com_msg.cur_yaw = next_yaw
         com_msg.cur_pitch = next_pitch
         com_msg.unix_time = time.time()
         
         self.pub_show.publish(com_msg)
-
         if node_decision_maker_mode == 'Dbg':
-            self.get_logger().debug(f"Choose Target {target_armor.name} id {target_armor.id} tvec {target_armor.tvec} rvec {target_armor.rvec} time {target_armor.time} ")
-        self.get_logger().debug(f"Make decision : target_abs_pitch {com_msg.cur_pitch:.3f} target_abs_yaw {com_msg.cur_yaw:.3f} reach_unix_time {com_msg.unix_time:.3f}")
+            self.get_logger().debug(f"Make decision : target_abs_pitch {com_msg.cur_pitch:.3f} target_abs_yaw {com_msg.cur_yaw:.3f} reach_unix_time {com_msg.unix_time:.3f}")
        
     def repeat_recv_from_ele_callback(self):
         
