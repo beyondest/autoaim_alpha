f5c387f54bcf1f77146dc1c118b32e9c4c245ddb
ggbond <liyuxuan12345678@outlook.com>
2024-03-14 08:21:46 -0400

    a

--- autoaim_alpha/autoaim_alpha/node_com.py
+++ autoaim_alpha/autoaim_alpha/node_com.py
@@ -24,7 +24,7 @@ class Node_Com(Node,Custom_Context_Obj):
                                                       self.listener_callback,
                                                       topic_electric_sys_com['qos_profile'])
 
-        self.timer_send_msg = self.create_timer(1/send_msg_freq, self.timer_send_msg_callback)
+        #self.timer_send_msg = self.create_timer(1/send_msg_freq, self.timer_send_msg_callback)
         self.timer_recv_msg = self.create_timer(1/recv_from_ele_sys_freq, self.timer_recv_msg_callback)
         
         self.port = Port(node_com_mode,
@@ -71,13 +71,10 @@ class Node_Com(Node,Custom_Context_Obj):
             if node_com_mode == 'Dbg':
                 self.get_logger().debug(f"SOF A from Decision maker : abs_pitch {msg.target_abs_pitch:.3f}, abs_yaw {msg.target_abs_yaw:.3f}, reach at time {msg.reach_unix_time:.3f}")
             
-            if msg.fire_times > 0:
-                self.port.send_msg(msg.sof)
-                self.port.action_data.fire_times = 0
-                
-                if node_com_mode == 'Dbg':
-                    self.get_logger().debug(f"Fire : {msg.fire_times} times")
-        
+            
+            self.port.send_msg(msg.sof)
+            self.port.action_data.fire_times = 0
+            
         elif msg.sof == 'S':
             cur_unix_time = time.time()
             minute, second, second_frac = TRANS_UNIX_TIME_TO_T(cur_unix_time, self.zero_unix_time)
