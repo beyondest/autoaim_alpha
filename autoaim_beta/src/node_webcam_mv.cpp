9940133ca4ed6c27f513e5171bac2653380cfba6
ggbond <liyuxuan12345678@outlook.com>
2024-03-23 09:27:22 -0400

    a

--- autoaim_beta/src/node_webcam_mv.cpp
+++ autoaim_beta/src/node_webcam_mv.cpp
@@ -7,8 +7,7 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "std_msgs/msg/string.hpp"
-#include "autoaim_interface/msg/armor_pos_list.hpp"
-
+#include "autoaim_interface/msg/detect_result.hpp"
 using namespace std::chrono_literals;
 
 class Node_Webcam_MV : public rclcpp::Node
@@ -17,7 +16,7 @@ class Node_Webcam_MV : public rclcpp::Node
 private:
 
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_ ;
-    rclcpp::Publisher<autoaim_interface::msg::ArmorPosList>::SharedPtr pub_armor_pos_list_;
+    rclcpp::Publisher<autoaim_interface::msg::DetectResult>::SharedPtr pub_detect_result_;
     rclcpp::TimerBase::SharedPtr timer_;
     int count_;
     void timer_callback()
@@ -35,7 +34,7 @@ public:
     {
         pub_= this->create_publisher<std_msgs::msg::String>("topic", 10);
         timer_ = this->create_wall_timer(500ms, std::bind(&Node_Webcam_MV::timer_callback, this));
-        
+
     }
     
 };
