53b2b1f462454a2175e330996928bf918ca65b3a
ggbond <liyuxuan12345678@outlook.com>
2024-03-28 07:23:08 -0400

    a

--- autoaim_beta/src/test/gg_test.cpp
+++ autoaim_beta/src/test/gg_test.cpp
@@ -1,7 +1,7 @@
 #include "gg_class.hpp"
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
+#include <chrono>
 int main()
 {
     GG_Camera gg(Mode::Dbg);
@@ -10,10 +10,15 @@ int main()
     gg.open();
     while (1)
     {
+        auto t1 = std::chrono::high_resolution_clock::now();
         gg.get_img(img);
         cv::resize(img,img_show,cv::Size(680,384));
         cv::imshow("img",img_show);
         cv::waitKey(1);
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+        auto fps = 1000000.0 / duration.count();
+        std::cout << "fps: " << fps << std::endl;
     }
     
 }
