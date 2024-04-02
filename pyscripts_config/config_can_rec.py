9defb3973c829611f5c14510e17c77b13c089a3f
ggbond <liyuxuan12345678@outlook.com>
2024-04-02 06:34:21 -0400

    a

--- pyscripts_config/config_can_rec.py
+++ pyscripts_config/config_can_rec.py
@@ -1,22 +1,26 @@
 import can
+import time
 from autoaim_alpha.port_slavedevice.com_tools import *
+from autoaim_alpha.port_slavedevice.port import *
 # 设置CAN接口名称
 interface_name = 'can0'
-
 # 创建一个CAN Bus接口
 bus = can.interface.Bus(channel=interface_name, bustype='socketcan_native')
+can_port = Can_Port('Dbg','/home/rcclub/ggbond/autoaim_ws/src/autoaim_alpha/config/other_config/port2_params.yaml')
 
 try:
     while True:
-        # 接收消息
-        message = bus.recv(timeout=1.0)
-        if message is not None:
-            # 解析并打印消息
-            received_data = message.data.decode()
-            print(f"Received message: {received_data}")
+        
+        i_error,f_yaw, c_yaw = can_port.recv_feedback()
+        # 等待1秒
+        print("i_error:{},f_yaw:{},c_yaw:{}".format(i_error,f_yaw,c_yaw))   
+        time.sleep(1)
 
 except KeyboardInterrupt:
     print("\nExiting...")
 finally:
     # 关闭CAN Bus接口
     bus.shutdown()
+
+
+
